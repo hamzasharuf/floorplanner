1e69ef067afe02a6dc226e5dabb2015e37c35b1f
Hamza Sharaf <hamzasharuf20@gmail.com>
2021-11-23 16:01:03 +0300

    Add demo app

--- app/src/main/java/com/hamzasharuf/floorplanner/MainActivity.kt
+++ app/src/main/java/com/hamzasharuf/floorplanner/MainActivity.kt
@@ -2,10 +2,21 @@ package com.hamzasharuf.floorplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.hamzasharuf.floor_planner.FloorPlannerView
+import com.hamzasharuf.floor_planner.model.Polygon
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fp = findViewById<FloorPlannerView>(R.id.floor_planner)
+        fp.setExtendedTouchRadius(50)
+        fp.setPadding(80.0)
+        fp.onCoordinatesUpdatedListener = object : FloorPlannerView.OnCoordinatesUpdatedListener {
+            override fun onCoordinatesUpdated(polygon: Polygon) {
+                Log.d("MainActivityLogs", "New Vertexes Coordinates => ${polygon.vertexes}")
+            }
+        }
     }
 }
